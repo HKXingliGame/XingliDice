d313f0f705c206b07000198545799f6852c4c3a5
HKXingli <matthias2wym@gmail.com>
2016-09-21 16:48:19 +0800

    Init Merge into Cordova

---
+++ XingliDice/www/scripts/dice.js
@@ -0,0 +1,80 @@
+// Extended Phaser.Sprite (check out the examples Sprites -> extending sprite demo 1 & 2)
+// Added a function to animate rolling.
+
+Dice = function (game, x, y) {
+
+    this.game = game;
+
+    Phaser.Sprite.call(this, this.game, x, y, 'dice');
+
+    this.tween;
+    this.anim;
+    this.blurX = this.game.add.filter("BlurX");  // Blur filters taken from
+    this.blurY = this.game.add.filter("BlurY");  // Filters -> blur example
+
+    // change the origin point from the top left (where Phaser puts it)
+    // to the centre
+    this.anchor.setTo(0.5, 0.5);
+
+    var i;
+    var frames = [];
+    for (i = 0; i < 15; i++) {
+        frames[i] = this.game.rnd.pick([0, 1, 2, 4, 5, 6]);
+    }
+
+    // the animation displays the frames from the spritesheet in a random order
+    this.anim = this.animations.add("roll", frames);
+    this.anim.onComplete.add(this.rollComplete, this);
+
+    this.frame = 1;
+
+    this.game.add.existing(this);
+};
+
+Dice.prototype = Object.create(Phaser.Sprite.prototype);
+Dice.prototype.constructor = Dice;
+
+
+Dice.prototype.roll = function () {
+    this.filters = [this.blurX, this.blurY];
+    this.animations.play("roll", 20);
+};
+
+Dice.prototype.rollComplete = function () {
+    this.filters = null;
+    this.frame = this.game.rnd.pick([0, 1, 2, 4, 5, 6]);
+};
+
+Dice.prototype.update = function () {
+    if (this.anim.isPlaying) {
+        this.angle = this.game.rnd.angle();
+    }
+};
+
+Dice.prototype.value = function () {
+
+    switch (this.frame) {
+        case 0:
+            return 6;
+            break;
+        case 1:
+            return 1;
+            break;
+        case 2:
+            return 2;
+            break;
+        case 4:
+            return 5;
+            break;
+        case 5:
+            return 3;
+            break;
+        case 6:
+            return 4;
+            break;
+        default:
+            return null;
+            break;
+    }
+};
+
--- XingliDice/www/scripts/index.js
+++ XingliDice/www/scripts/index.js
@@ -5,26 +5,57 @@
 (function () {
     "use strict";
 
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+    var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, "dice", gameRoute);
+    var diceGroup;
+    var text;
 
-    function onDeviceReady() {
-        // Handle the Cordova pause and resume events
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
-        
-        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-        var parentElement = document.getElementById('deviceready');
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+    var gameRoute = {
+
+
+        preload: function () {
+
+            // centre the canvas
+            this.game.load.spritesheet("dice", "images/dice.png", 64, 64);
+            this.game.load.script("BlurX", "scripts/BlurX.js");
+            this.game.load.script("BlurY", "scripts/BlurY.js");
+        },
+
+
+        create: function () {
+
+            diceGroup = this.game.add.group();
+
+            var i;
+            for (i = 0; i < 5; i++) {
+                var d = new Dice(this.game, i * 100 + 90, 190);
+                diceGroup.add(d);
+            }
+
+            // roll the dice when a mouse button is clicked
+            this.game.input.onDown.add(
+            function () {
+                var total = 0;
+                diceGroup.callAll("roll", null);
+            });
+
+
+            text = this.game.add.text(30, 90, "Total: ");
+            text.fontSize = 30;
+            text.fill = "#d3d3d3";
+
+        },
+
+        update: function () {
+            // I don't like having the foreach code run so often.
+            // Ideally it would only be run when the dice have finished
+            // rolling, but I haven't worked out how to do that yet.
+            var total = 0;
+            diceGroup.forEach(function (item) { total += item.value(); });
+            text.setText("Total: " + total);
+        },
     };
 
-    function onPause() {
-        // TODO: This application has been suspended. Save application state here.
-    };
+    game.state.add('gameRoute', gameRoute);
+    game.state.start('gameRoute');
 
-    function onResume() {
-        // TODO: This application has been reactivated. Restore application state here.
-    };
-} )();
+})();
